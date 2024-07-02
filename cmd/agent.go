2316bed4b7f7dbd226a3741a36098c55b1ca6fa9
Evan Azevedo <me@evanazevedo.com>
2024-07-02 13:10:21 -0600

    main local work

--- cmd/agent.go
+++ cmd/agent.go
@@ -1,13 +1,38 @@
-package agent
+package main
 
 
-type Agent struct {
-    Name string
+type Move int8
+
+const (
+    Cheat Move = iota
+    Cooperate
+)
+
+type cpuAgent struct {
+    name string
 }
 
-func newAgent(name string) Agent {
-    return Agent{
-        Name: name,
+type agent interface {
+    act() Move
+}
+
+type BattleResults struct {
+    TotalRounds int
+    AgentName string
+    AgentWins int
+    AgentWinPct float32
+    OpponentName string
+    OpponentWins int
+    OpponentWinPct float32
+}
+
+func battle(agent1, agent2 agent, nrounds int) BattleResults {
+    for round:=0; round<nrounds; round++ {
+        println()
+    }
+    return BattleResults{
+        TotalRounds: nrounds,
+        AgentName: agent1.Name,
     }
 }
 
