894033d8254cb823b58b5a7f1eeb5b041316a748
Evan Azevedo <me@evanazevedo.com>
2024-07-23 17:05:53 -0600

    Working on stuff

--- cmd/agent.go
+++ cmd/agent.go
@@ -1,34 +1,33 @@
 package main
 
 import (
-    "errors"
-    "math/rand"
+	"errors"
+	"math/rand"
 )
-
 
 type Move int8
 type MoveHistory []Move
 
 const (
-    Cheat Move = iota
-    Cooperate
+	Cheat Move = iota
+	Cooperate
 )
 
 func inverseMove(move Move) Move {
-    if move == Cheat {
-        return Cooperate
-    } else {
-        return Cheat
-    }
+	if move == Cheat {
+		return Cooperate
+	} else {
+		return Cheat
+	}
 }
 
 type Agent struct {
-    Name string
+	Name string
 }
 
 // An interface to the action of an Agent
 type Actor interface {
-    act(agentHistory, oppHistory MoveHistory) (Move, error)
+	act(agentHistory, oppHistory MoveHistory) (Move, error)
 }
 
 // Define different default agents
@@ -40,124 +39,125 @@ type grudger Agent
 type detective Agent
 
 func (a quidProQuo) act(agentHistory, oppHistory MoveHistory) (Move, error) {
-    var nHistory int = len(oppHistory)
-    if nHistory == 0 {
-        return Cooperate, nil
-    }
+	var nHistory int = len(oppHistory)
+	if nHistory == 0 {
+		return Cooperate, nil
+	}
 
-    switch mostRecent := oppHistory[nHistory-1]; mostRecent {
-        case Cheat:
-            return Cheat, nil
-        case Cooperate:
-            return Cooperate, nil
-        default:
-            return 0, errors.New("Invalid move in move history")
-    }
+	switch mostRecent := oppHistory[nHistory-1]; mostRecent {
+	case Cheat:
+		return Cheat, nil
+	case Cooperate:
+		return Cooperate, nil
+	default:
+		return 0, errors.New("Invalid move in move history")
+	}
 }
 
 // An agent that always cooperates
 func (a alwaysCooperate) act(agentHistory, oppHistory MoveHistory) (Move, error) {
-    return Cooperate, nil
+	return Cooperate, nil
 }
 
 // An agent that always cheats
 func (a alwaysCheat) act(agentHistory, oppHistory MoveHistory) (Move, error) {
-    return Cheat, nil
+	return Cheat, nil
 }
 
 // A copycat agent
 func (a copycat) act(agentHistory, oppHistory MoveHistory) (Move, error) {
-    return Cheat, nil
+	return Cheat, nil
 }
 
 // A grudger agent
 func (a grudger) act(agentHistory, oppHistory MoveHistory) (Move, error) {
-    return Cheat, nil
+	return Cheat, nil
 }
 
 // A detective agent
 func (a detective) act(agentHistory, oppHistory MoveHistory) (Move, error) {
-    return Cheat, nil
+	return Cheat, nil
 }
 
 type BattleResults struct {
-    Agent1 Actor
-    Agent1MoveHistory []Move
-    Agent1ScoreHistory []int
-    Agent1ScoreTotal int
-    Agent2 Actor
-    Agent2MoveHistory []Move
-    Agent2ScoreHistory []int
-    Agent2ScoreTotal int
+	Agent1             Actor
+	Agent1MoveHistory  []Move
+	Agent1ScoreHistory []int
+	Agent1ScoreTotal   int
+	Agent2             Actor
+	Agent2MoveHistory  []Move
+	Agent2ScoreHistory []int
+	Agent2ScoreTotal   int
 }
 
 type BattleParams struct {
-    CooperateReward int
-    CheatReward int
-    Opacity float32
-    NRounds int
+	CooperateReward int
+	CheatReward     int
+	Opacity         float32
+	NRounds         int
 }
 
 // Calculate the score of the round for each agent
 func score(agent1Move, agent2Move Move, params BattleParams) (agent1Score, agent2Score int) {
-    switch {
-    case agent1Move == Cooperate && agent2Move == Cooperate:
-        output := 1 + params.CooperateReward
-        return output, output
-    case agent1Move == Cooperate && agent2Move == Cheat:
-        return -1, 1 + params.CheatReward
-    case agent1Move == Cheat && agent2Move == Cooperate:
-        return 1 + params.CheatReward, -1
-    default:
-        return 0, 0
-    }
+	switch {
+	case agent1Move == Cooperate && agent2Move == Cooperate:
+		output := 1 + params.CooperateReward
+		return output, output
+	case agent1Move == Cooperate && agent2Move == Cheat:
+		return -1, 1 + params.CheatReward
+	case agent1Move == Cheat && agent2Move == Cooperate:
+		return 1 + params.CheatReward, -1
+	default:
+		return 0, 0
+	}
 }
 
 // Roll the possibility that the intended move gets obfuscated
 func rollObfuscate(inputMove Move, p float32) Move {
-    var roll = rand.Float32()
-    if roll > p {
-        return inputMove
-    } else {
-        var outputMove = inverseMove(inputMove)
-        return outputMove
-    }
+	var roll = rand.Float32()
+	if roll > p {
+		return inputMove
+	} else {
+		var outputMove = inverseMove(inputMove)
+		return outputMove
+	}
 }
 
 // Execute the battle
 func battle(agent1, agent2 Actor, params BattleParams) BattleResults {
-    var agent1MoveHistory []Move
-    var agent1ScoreHistory []int
-    var agent1ScoreTotal int = 0
+	var agent1MoveHistory []Move
+	var agent1ScoreHistory []int
+	var agent1ScoreTotal int = 0
 
-    var agent2MoveHistory []Move
-    var agent2ScoreHistory []int
-    var agent2ScoreTotal int = 0
+	var agent2MoveHistory []Move
+	var agent2ScoreHistory []int
+	var agent2ScoreTotal int = 0
 
-    for round:=0; round<params.NRounds; round++ {
-        agent1Move, _ := agent1.act(agent1MoveHistory, agent2MoveHistory)
-        agent1MoveHistory = append(agent1MoveHistory, agent1Move)
+	for round := 0; round < params.NRounds; round++ {
+		agent1Move, _ := agent1.act(agent1MoveHistory, agent2MoveHistory)
+		agent1Move = rollObfuscate(agent1Move, params.Opacity)
+		agent1MoveHistory = append(agent1MoveHistory, agent1Move)
 
-        agent2Move, _ := agent2.act(agent2MoveHistory, agent1MoveHistory)
-        agent2MoveHistory = append(agent1MoveHistory, agent2Move)
+		agent2Move, _ := agent2.act(agent2MoveHistory, agent1MoveHistory)
+		agent2Move = rollObfuscate(agent2Move, params.Opacity)
+		agent2MoveHistory = append(agent1MoveHistory, agent2Move)
 
-        agent1Score, agent2Score := score(agent1Move, agent2Move, params)
-        agent1ScoreHistory = append(agent1ScoreHistory, agent1Score)
-        agent1ScoreTotal += agent1Score
+		agent1Score, agent2Score := score(agent1Move, agent2Move, params)
+		agent1ScoreHistory = append(agent1ScoreHistory, agent1Score)
+		agent1ScoreTotal += agent1Score
 
-        agent2ScoreHistory = append(agent2ScoreHistory, agent2Score)
-        agent2ScoreTotal += agent2Score
-    }
+		agent2ScoreHistory = append(agent2ScoreHistory, agent2Score)
+		agent2ScoreTotal += agent2Score
+	}
 
-    return BattleResults{
-        Agent1: agent1,
-        Agent1MoveHistory: agent1MoveHistory,
-        Agent1ScoreHistory: agent1ScoreHistory,
-        Agent1ScoreTotal: agent1ScoreTotal,
-        Agent2: agent2,
-        Agent2MoveHistory: agent2MoveHistory,
-        Agent2ScoreHistory: agent1ScoreHistory,
-        Agent2ScoreTotal: agent2ScoreTotal,
-    }
+	return BattleResults{
+		Agent1:             agent1,
+		Agent1MoveHistory:  agent1MoveHistory,
+		Agent1ScoreHistory: agent1ScoreHistory,
+		Agent1ScoreTotal:   agent1ScoreTotal,
+		Agent2:             agent2,
+		Agent2MoveHistory:  agent2MoveHistory,
+		Agent2ScoreHistory: agent2ScoreHistory,
+		Agent2ScoreTotal:   agent2ScoreTotal,
+	}
 }
-
--- cmd/main.go
+++ cmd/main.go
@@ -1,85 +1,86 @@
 package main
 
 import (
-	"io"
 	"html/template"
+	"io"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 type Templates struct {
-    templates *template.Template
+	templates *template.Template
 }
 
 func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-    return t.templates.ExecuteTemplate(w, name, data)
+	return t.templates.ExecuteTemplate(w, name, data)
 }
 
 func newTemplate() *Templates {
-    return &Templates{
-        templates: template.Must(template.ParseGlob("views/*.html")),
-    }
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
 }
 
 type Battle struct {
-    OpponentName string
-    Score int
-    ScoreHistory []int
+	OpponentName string
+	Score        int
+	ScoreHistory []int
 }
 
 type Battles = []Battle
 
 type Data struct {
-    Agent Actor
-    Battles Battles
+	Agent   Actor
+	Battles Battles
 }
 
 func runBattles(agent Actor, opponents []Actor, params BattleParams) Battles {
-    var nOpponents int = len(opponents)
-    var battleResults = make([]Battle, nOpponents)
-    for i, opponent := range opponents {
-        result := battle(agent, opponent, params)
-        battleResult := Battle{
-            OpponentName: opponent.Name,
-            Score: result.Agent1ScoreTotal,
-            ScoreHistory: result.Agent1ScoreHistory,
-        }
-        battleResults[i] = battleResult
-    }
-    return battleResults
+	var nOpponents int = len(opponents)
+	var battleResults = make([]Battle, nOpponents)
+
+	for i, opponent := range opponents {
+		result := battle(agent, opponent, params)
+		opponentAgent := opponent.(quidProQuo)
+		battleResult := Battle{
+			OpponentName: opponentAgent.Name,
+			Score:        result.Agent1ScoreTotal,
+			ScoreHistory: result.Agent1ScoreHistory,
+		}
+		battleResults[i] = battleResult
+	}
+	return battleResults
 }
 
 func main() {
-    e := echo.New()
-    e.Use(middleware.Logger())
+	e := echo.New()
+	e.Use(middleware.Logger())
 
-    params := BattleParams{
-        CooperateReward: 1,
-        CheatReward: 2,
-        Opacity: 0.05,
-        NRounds: 200,
-    }
+	params := BattleParams{
+		CooperateReward: 1,
+		CheatReward:     2,
+		Opacity:         0.05,
+		NRounds:         200,
+	}
 
-    var agent = quidProQuo{"Quid Pro Quo"}
-    var opponents = make([]Actor, 5)
-    opponents[0] = alwaysCheat{"Always Cheat"}
-    opponents[1] = alwaysCooperate{"Always Cooperate"}
-    opponents[2] = copycat{"Copycat"}
-    opponents[3] = grudger{"Grudger"}
-    opponents[4] = detective{"Detective"}
+	var agent = quidProQuo{"Quid Pro Quo"}
+	var opponents = make([]Actor, 5)
+	opponents[0] = alwaysCheat{"Always Cheat"}
+	opponents[1] = alwaysCooperate{"Always Cooperate"}
+	opponents[2] = copycat{"Copycat"}
+	opponents[3] = grudger{"Grudger"}
+	opponents[4] = detective{"Detective"}
 
-    battleResults := runBattles(agent, opponents, params)
+	battleResults := runBattles(agent, opponents, params)
 
-    data := Data{
-        Agent: agent,
-        Battles: battleResults,
-    }
-    e.Renderer = newTemplate()
+	data := Data{
+		Agent:   agent,
+		Battles: battleResults,
+	}
+	e.Renderer = newTemplate()
 
-    e.GET("/", func(c echo.Context) error {
-        return c.Render(200, "index", data)
-    })
-    e.Logger.Fatal(e.Start(":2000"))
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", data)
+	})
+	e.Logger.Fatal(e.Start(":2000"))
 }
-
